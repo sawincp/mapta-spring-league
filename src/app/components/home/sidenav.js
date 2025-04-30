b0aa6f1e189ed69c3d1b30d5a26c9f43642d0fe8
coreysawin <sawincp@gmail.com>
2025-04-30 16:29:20 -0400

    add navlinks

---
+++ src/app/components/home/sidenav.js
@@ -0,0 +1,22 @@
+"use client";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+
+export default function Sidenav() {
+  return (
+    <Nav defaultActiveKey="/" className="flex-column">
+      <Button variant="outline-light">
+        <Nav.Link href="/">Home</Nav.Link>
+      </Button>
+      <Button variant="outline-light">
+        <Nav.Link eventKey="link-1">Teams</Nav.Link>
+      </Button>
+      <Button variant="outline-light">
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Button>
+      <Button variant="outline-light">
+        <Nav.Link eventKey={"link-3"}>Link</Nav.Link>
+      </Button>
+    </Nav>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidenav from "./components/home/sidenav";
 
 
 const geistSans = Geist({
@@ -22,8 +23,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Sidenav />
         {children}
       </body>
     </html>
